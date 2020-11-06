c9b5f3f82c421f90e04e7a42dea21fdb0e0b3a16
Vinh <vjhnguyen@gmail.com>
2020-11-06 14:19:17 +0700

    update

--- pillow/doJob.py
+++ pillow/doJob.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os, sys
 
-path = "/Users/vinhn/Desktop/fix2/"
+path = "/Users/vinhn/Google Drive/xmas design out put/upload files/06/"
 dirs = os.listdir( path )
 print(dirs)
 
@@ -9,7 +9,7 @@ def resize():
     for item in dirs:
         im = Image.open(path+item)
         f, e = os.path.splitext(path+item)
-        imResize = im.resize((3300,4600), Image.ANTIALIAS)
+        imResize = im.resize((2400,3125), Image.ANTIALIAS)
         imResize.save(f + ' resized.png', 'PNG', quality=90)
 
 resize()
